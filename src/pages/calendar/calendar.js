106e4d0fdb6a4b7fdbd3df57a1b4379084125b71
Jason Bean <jasonbean92@gmail.com>
2022-02-08 17:35:06 -0500

    Calendar test

--- src/pages/calendar/calendar.js
+++ src/pages/calendar/calendar.js
@@ -1,4 +1,4 @@
-const events = [
+const CalEvents = [
   { title: "new event", start: "2022-02-02" },
   { title: "new event", start: "2022-16-02" },
   { title: "new event", start: "2022-09-02" },
@@ -8,21 +8,11 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
   var calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
-    dateClick: function (info) {
-      alert("Clicked on: " + info.dateStr);
-      alert("Coordinates: " + info.jsEvent.pageX + "," + info.jsEvent.pageY);
-      alert("Current view: " + info.view.type);
-      // change the day's background color just for fun
-      info.dayEl.style.backgroundColor = "red";
-    },
-  });
-  calendar.batchRendering(function () {
-    calendar.changeView("dayGridMonth");
-    calendar.addEvent({
-      title: "new event",
-      start: "2022-02-12",
-      end: "2022-02-15",
-    });
+    events: [
+      { title: "new event", start: "2022-02-02" },
+      { title: "Event 2", start: "2022-02-23" },
+      { title: "test", start: "2022-02-12" },
+    ],
   });
   calendar.render();
 });
